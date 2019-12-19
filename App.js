386fba5047f9f7d473b8ea5ce2c12f78cd323de3
Rolli Prabu Dwilaksana <rolli@prisma-solusi.com>
2019-12-19 13:55:07 +0700

    Native Toast & burger menu animation

--- App.js
+++ App.js
@@ -9,7 +9,6 @@
 import React, {Component} from 'react';
 import {
   Animated,
-  Button,
   Dimensions,
   ScrollView,
   StyleSheet,
@@ -18,14 +17,15 @@ import {
   TouchableOpacity,
   View
 } from 'react-native';
-import { Avatar, Badge, Icon, withBadge } from 'react-native-elements';
+import { Badge, Icon } from 'react-native-elements';
+import { ToastModule } from "./module"
 
 const { width } = Dimensions.get('window');
 
 class BannerList extends Component{
   render(){
     return(
-      <View key={this.props.keyID} style={{height: 100, width: 300, backgroundColor: this.props.color, marginHorizontal: 15}}></View>
+      <View key={this.props.keyID} style={[{backgroundColor: this.props.color}, styles.banner]}></View>
     )
   }
 }
@@ -44,7 +44,7 @@ class DotIndicator extends Component{
     return(
       <Animated.View
         key={this.props.keyID}
-        style={{opacity, height: 10, width: 10, backgroundColor: '#595959', marginHorizontal: 8, borderRadius: 5 }}
+        style={[{opacity}, styles.dotIndicator]}
       />
     )
   }
@@ -59,7 +59,11 @@ class ShoesList extends Component{
           onPress={() => this.props.func()}
           style={{flexDirection: 'row', padding: 10, backgroundColor: 'grey'}}
         >
-          <View style={{height: 20, width: 20, backgroundColor: 'white'}}></View>
+          <Icon
+            type='antdesign'
+            name='plussquareo'
+            color='white'
+          />
           <Text style={{paddingLeft: 10, color: 'white'}}>{this.props.shoeName}</Text>
         </TouchableOpacity>
       </View>      
@@ -67,38 +71,96 @@ class ShoesList extends Component{
   }
 }
 
+class Fade extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visible: props.visible,
+    };
+  };
+
+  componentWillMount() {
+    this._visibility = new Animated.Value(this.props.visible ? 1 : 0);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.visible) {
+      this.setState({ visible: true });
+    }
+    Animated.timing(this._visibility, {
+      toValue: nextProps.visible ? 1 : 0,
+      duration: 75,
+    }).start(() => {
+      this.setState({ visible: nextProps.visible });
+    });
+  }
+
+  render() {
+    const { visible, style, children, ...rest } = this.props;
+
+    const containerStyle = {
+      opacity: this._visibility.interpolate({
+        inputRange: [0, 1],
+        outputRange: [0, 1],
+      }),
+      transform: [
+        {
+          scale: this._visibility.interpolate({
+            inputRange: [0, 1],
+            outputRange: [1.1, 1],
+          }),
+        },
+      ],
+    };
+
+    const combinedStyle = [containerStyle, style];
+    return (
+      <Animated.View style={this.state.visible ? combinedStyle : containerStyle} {...rest}>
+        {this.state.visible ? children : null}
+      </Animated.View>
+    );
+  }
+}
+
 export default class App extends Component {
   constructor(props){
     super(props);
     this.state={
-      cartItem: 0
+      cartItem: 0,
+      visibility: true
     }
   }
 
   scrollX = new Animated.Value(0)
 
-  setCartItem(){
+  setCartItem(shoesname){
     this.setState({cartItem: this.state.cartItem+1})
+    ToastModule.toastMe(`${shoesname} added to the cart`, ToastModule.SHORT)
   }
 
   render(){
     let position = Animated.divide(this.scrollX, width);
-    // const BadgedIcon = withBadge(this.state.cartItem)(Icon)
 
     return(
       <View>
         <View style={{flexDirection: 'row', marginBottom: 10, height: '10%', justifyContent: 'space-between'}}>
           <View style={{justifyContent:'center', height: 70, width: 70}}>
-            <Icon
-              type='Feather'
-              name='menu'
-            />
+            <TouchableOpacity onPress={() => this.setState({visibility: !this.state.visibility})}>
+              <Fade visible={this.state.visibility}>
+                <Icon
+                  type='feather'
+                  name='menu'
+                />
+              </Fade>
+              <Fade visible={!this.state.visibility}>
+                <Icon
+                  type='feather'
+                  name='x'
+                />
+              </Fade>
+            </TouchableOpacity>                     
           </View>
           <View style={{paddingRight: 20, paddingTop: 20, justifyContent:'center', height: 50, width: 50}}>
-            {/* <BadgedIcon
-              type="EvilIcons"
-              name="shopping-cart"
-            /> */}
             <Icon
               type="EvilIcons"
               name="shopping-cart"
@@ -111,7 +173,7 @@ export default class App extends Component {
           </View>
         </View>
 
-        <Text style={{fontWeight: 'bold', fontSize: 24, paddingLeft: 20, marginBottom: 20, height: '5%'}}>Nike App Store</Text>
+        <Text style={styles.titleText}>Nike App Store</Text>
 
         <View style={{height: 100, marginBottom: 5}}>
           <ScrollView
@@ -143,10 +205,10 @@ export default class App extends Component {
 
         <View style={{height: '45%'}}>
           <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-            <ShoesList color={'purple'} shoeName={"Nike a"} func={() => {this.setCartItem()}}/>
-            <ShoesList color={'green'} shoeName={"bata"} func={() => {this.setCartItem()}}/>
-            <ShoesList color={'blue'} shoeName={"aid ideas"} func={() => {this.setCartItem()}}/>
-            <ShoesList color={'red'} shoeName={"wacky"} func={() => {this.setCartItem()}}/>
+            <ShoesList color={'purple'} shoeName={"Nike a"} func={() => {this.setCartItem("nike a")}}/>
+            <ShoesList color={'green'} shoeName={"bata"} func={() => {this.setCartItem("bata")}}/>
+            <ShoesList color={'blue'} shoeName={"aid ideas"} func={() => {this.setCartItem("aid ideas")}}/>
+            <ShoesList color={'red'} shoeName={"wacky"} func={() => {this.setCartItem("wacky")}}/>
           </ScrollView>
         </View>
         
@@ -157,7 +219,23 @@ export default class App extends Component {
 }
 
 const styles = StyleSheet.create({
-  scrollView: {
-    flex: 1
+  titleText: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    paddingLeft: 20,
+    marginBottom: 20,
+    height: '5%'
+  },
+  banner: {
+    height: 100,
+    width: 300,
+    marginHorizontal: 15
+  },
+  dotIndicator: {
+    height: 10,
+    width: 10,
+    backgroundColor: '#595959',
+    marginHorizontal: 8,
+    borderRadius: 5
   }
 });
